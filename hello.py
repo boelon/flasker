def6c4629c7f4593792a58739a9d0a3f0b1235bc
boelon <boelo.niemeijer@gmail.com>
2023-05-07 20:42:15 +0800

    added some css and js

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key"
+
+#Create a form class
+class NamerForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 #Route decorator
 @app.route('/')
@@ -11,6 +21,7 @@ app = Flask(__name__)
 def index():
     first_name="John"
     stuff = "This is <strong>bold</strong> text."
+    flash("Welcome To Our Website!")
     title = "bob the blog builder"
     favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
     return render_template('index.html',
@@ -31,3 +42,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+
+#Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+
+    return render_template('name.html',
+                           name = name,
+                           form = form)
