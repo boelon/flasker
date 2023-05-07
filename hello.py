234bfd974fa9c546fd2285eed209b8d177b48135
boelon <boelo.niemeijer@gmail.com>
2023-05-07 17:06:15 +0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+#Flask instance
+app = Flask(__name__)
+
+#Route decorator
+@app.route('/')
+#def index():
+#   return "<h1>Hello World!</h1>"
+
+def index():
+    first_name="John"
+    stuff = "This is <strong>bold</strong> text."
+    title = "bob the blog builder"
+    favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+    return render_template('index.html',
+                           first_name=first_name,
+                           stuff=stuff,
+                           title=title,
+                           favorite_pizza=favorite_pizza)
+
+#localhost:5000/user/john
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
