19694ac20b83103c046008f67033112508c45c16
boelon <boelo.niemeijer@gmail.com>
2023-05-14 22:41:49 +0800

    added db mysqllite

--- hello.py
+++ hello.py
@@ -2,26 +2,56 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 #Flask instance
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
 app.config['SECRET_KEY'] = "my secret key"
+app.app_context().push()
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db. Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 #Create a form class
-class NamerForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 #Route decorator
-@app.route('/')
-#def index():
-#   return "<h1>Hello World!</h1>"
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash('User Added Successfully')
+    our_users = Users.query.order_by(Users.date_added)  
+    return render_template('add_user.html', form=form, name=name, our_users=our_users)
 
+@app.route('/')
 def index():
     first_name="John"
     stuff = "This is <strong>bold</strong> text."
-    flash("Welcome To Our Website!")
+    #flash("Welcome To Our Website!")
     title = "bob the blog builder"
     favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
     return render_template('index.html',
@@ -48,7 +78,7 @@ def page_not_found(e):
 @app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None
-    form = NamerForm()
+    form = UserForm()
     #validate form
     if form.validate_on_submit():
         name = form.name.data
